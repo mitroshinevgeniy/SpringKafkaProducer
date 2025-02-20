c1c82e9022e9868e565056077454157e723944cd
Evgeniy Mitroshin <mitroshin.evgeniy@gmail.com>
2025-02-20 16:19:45 +0400

    configurie producer

--- src/main/java/ru/mitroshin/springKafkaProducer/config/KafkaProducerConfig.java
+++ src/main/java/ru/mitroshin/springKafkaProducer/config/KafkaProducerConfig.java
@@ -3,6 +3,7 @@ package ru.mitroshin.springKafkaProducer.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -17,25 +18,43 @@ import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
-    @Bean
-    NewTopic createTopic(){
-        return TopicBuilder.name("product-topic")
-                .partitions(3)
-                .replicas(3)
-                .configs(Map.of("min.insync.replicas", "2"))
-                .build();
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String  bootstrap_servers;
+    @Value("${app.kafka.topic.my-topic}")
+    private String topic;
+
+    Map<String, Object> producerConfig(){
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap_servers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        config.put(ProducerConfig.ACKS_CONFIG, "all");
+        config.put(ProducerConfig.RETRIES_CONFIG, 3);
+        config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, 10000);
+        config.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 5000);
+
+        return  config;
     }
+
     @Bean
     public ProducerFactory<String, ProductCreateEvent> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092, localhost:39092, localhost:49092");
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
+        return new DefaultKafkaProducerFactory<>(producerConfig());
     }
 
     @Bean
     public KafkaTemplate<String, ProductCreateEvent> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
+
+    @Bean
+    NewTopic createTopic(){
+        return TopicBuilder.name(topic)
+                .partitions(3)
+                .replicas(3)
+                .configs(Map.of("min.insync.replicas", "2"))
+                .build();
+    }
+
 }
